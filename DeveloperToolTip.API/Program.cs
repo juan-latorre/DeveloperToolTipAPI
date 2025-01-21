6cc19271d0ef22fc1c680d19d1ec08fb6eafca9d
juan-latorre <juanlatorrevidal@gmail.com>
2025-01-21 16:43:25 -0500

    se modifica el program.cs

--- DeveloperToolTip.API/Program.cs
+++ DeveloperToolTip.API/Program.cs
@@ -51,7 +51,6 @@ builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices();
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
